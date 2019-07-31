4c2d778a82ef240190aa2897ce194d27ff99ae6b
wangyanlin01 <wangyanlin01@daojia-inc.com>
2019-07-31 18:59:58 +0800

    合并冲突

--- gitDemo/main/com.wyl.Servlet/TestClall.java
+++ gitDemo/main/com.wyl.Servlet/TestClall.java
@@ -4,7 +4,5 @@ public class TestClall {
     public static void main(String[] args) {
         System.out.println("AAAAAA");
     }
-    public static void main(String[] args) {
-        System.out.println("hahaha");
-    }
+
 }
